eddf6a4d12b1b44d8d05b9d61924b35bfa0ecbcf
Joshua Theze <foxwolfblood1@yahoo.com>
2013-03-24 02:10:43 -0400

    Heavy modifications to core, read changelog.markdown for more info.

---
+++ changelog.markdown
@@ -0,0 +1,15 @@
+Mar 24, 2013
+============
+foxiepaws - Heavy changes to the core, Pretty Much an entire rewrite
+
+  * Now using GetOpt to allow passing config files and verbose/debug modes
+  * Added in SSL support
+  * Reorder and restructure of the code.
+  * default handler for verboseprint mode
+
+Oct 1, 2012
+===========
+Staticsafe - Added the Initial design for DemonReach
+Sept 28, 2012
+=============
+Staticsafe - Changed readme
--- core.pl
+++ core.pl
@@ -1,53 +1,103 @@
-#!/usr/bin/env perl
-use 5.16.1;
-use strict;
-use warnings;
-use POE qw(Component::IRC);
+#use IRC::Message::Object;
+use POE qw(Component::IRC Component::SSLify);
 use YAML qw(LoadFile);
+use Getopt::Long;
 use Data::Dumper;
 
-# hardcoding location of config file, feel free to change
-my $configfilename = "config.yml";
+my $debug = '';
+my $verbose = '';
+my $configFile = 'config.yml';
 
-# load YAML stream into perl data structures
-my ($yamldata) = LoadFile("$configfilename");
-
-# initializing essential variables
-my $hostname = $yamldata->{hostname};
-my $port = $yamldata->{port};
-my $nick = $yamldata->{nick};
-my $ircname = $yamldata->{ircname};
-my $username = $yamldata->{username};
-
-# Start IRC Connection (code borrowed from the module docs)
-my $irc = POE::Component::IRC->spawn(
-    nick => $nick,
-    ircname => $ircname,
-    port => $port,
-    server => $hostname,
-    username => $username,
-) or die "Could not spawn POE component $!";
-
-POE::Session->create(
-    package_states => [
-        main => [ qw(_start irc_001) ],
-    ],
-    heap => { irc => $irc },
+GetOptions(
+    'verbose|v' => \$verbose, 
+    'debug|d' => \$debug, 
+    'c|config=s' => \$configFile
 );
 
-$poe_kernel->run();
+sub verbprint {
+    my $stuff = shift;
+    print $stuff . "\n" if $verbose;   
+}
 
-sub _start{
+sub debugprint {
+    my $stuff = shift;
+    print $stuff . "\n" if $debug;
+}
+
+debugprint "Debug mode activated";
+verbprint "Verbose mode activated";
+verbprint "Using config file: $configFile";
+
+verbprint "Loading Config File";
+my ($configdata) = LoadFile($configFile);
+
+# for debug, dump the YAML object.
+debugprint Dumper $configdata;
+
+# 
+my $hostname = $configdata->{hostname};
+my $port     = $configdata->{port};
+my $nick     = $configdata->{nick};
+my $realname = $configdata->{realname};
+my $username = $configdata->{username};
+verbprint "Connecting to $hostname:$port as '$nick!~$username' with realname '$realname'";
+# if SSL is 'on', set $SSL to 1, otherwise, 0
+my $ssl = 0;
+if ($configdata->{ssl} eq 'on') {
+    $ssl = 1;
+} 
+verbprint ((sub {if ($ssl == 1) { return "SSL Enabled" } else { return "SSL Disabled" }})->());
+
+# get Oper creds.
+my $operuser = $configdata->{oper}->{username};
+my $operpass = $configdata->{oper}->{password};
+
+# POE::IRC stuff
+my $irc = POE::Component::IRC->spawn(
+        nick => $nick, 
+        ircname => $realname,
+        port=> $port, 
+        server => $hostname, 
+        username => $username,
+        UseSSL => $ssl
+    ) or die("Couldn't spawn POE component $!");
+
+# start events
+sub _start {
     my $heap = $_[HEAP];
+    # retrieve our component's object from the heap where we stashed it
     my $irc = $heap->{irc};
     $irc->yield( register => 'all' );
     $irc->yield( connect => { } );
     return;
 }
 
-sub irc_001 {
-    my $sender = $_[SENDER];
-    my $irc = $sender->get_heap();
-    print "Connected to ", $irc->server_name(), "\n";
+# default handler
+sub _default {
+    my ($event, $args) = @_[ARG0 .. $#_];
+    my @output = ( "$event: " );
+    for my $arg (@$args) {
+        if ( ref $arg eq 'ARRAY' ) {
+            push( @output, '[' . join(', ', @$arg ) . ']' );
+        } else {
+            push ( @output, "'$arg'" );
+        }
+    }
+    verbprint join ' ', @output;
     return;
 }
+
+
+
+
+# POE Session 
+POE::Session->create(
+    package_states => [
+        main => [ qw(_default _start) ],
+    ],
+    heap => { irc => $irc },
+);
+
+# run the fucking bot.
+$poe_kernel->run();
+
